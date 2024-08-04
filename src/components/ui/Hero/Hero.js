5fc02c8277a5fa1dca62ac27d58943308eccc044
Rizky Nugraha <rizkynp990@gmail.com>
2024-08-04 22:01:32 +0700

    add parallax text & change dock

--- src/components/ui/Hero/Hero.js
+++ src/components/ui/Hero/Hero.js
@@ -1,6 +1,6 @@
 import TypingAnimation from "@/components/magicui/typing-animation"
 import React from "react";
-import {DockDemo} from "@/components/ui/Dock/DockDemo";
+import { DockDemo } from "@/components/ui/Dock/DockDemo";
 import OrbitingCircle from "../Orbit/OrbitingCircle";
 
 export default function Hero() {
@@ -11,7 +11,7 @@ export default function Hero() {
     ];
     return (
         <section className="min-h-screen bg-back1 p-4 md:px-8">
-            <div className="grid grid-cols-1 lg:grid-cols-2 mt-28 gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 mt-16 lg:mt-28 gap-4">
                 <div className="w-full px-4 flex flex-col justify-center items-center ">
                     <TypingAnimation
                         className="text-2xl font-bold text-primary text-center lg:text-4xl "
@@ -19,10 +19,10 @@ export default function Hero() {
                         duration={100}
                         pauseBetweenAnimations={500}
                     />
-                    <DockDemo/>
+                    <DockDemo />
                 </div>
                 <div className="w-full flex justify-center items-center">
-                    <OrbitingCircle/>
+                    <OrbitingCircle />
                 </div>
             </div>
         </section>
